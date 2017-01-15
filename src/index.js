de6acc5b0a056ae3a35d59257e704a36e5a6243b
Greg Wiley <aztec.rex@jammm.com>
2017-01-15 11:40:01 -0800

    initialize redux store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
+const reduce = x => x;
+
+const store = createStore(reduce);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
