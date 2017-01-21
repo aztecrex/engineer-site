7da2b9ff13eec47ad83b7eb0e1f97ed5b2c61569
Greg Wiley <aztec.rex@jammm.com>
2017-01-20 22:12:37 -0800

    generate markdown files from articles

---
+++ tool/prep-articles.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const FrontMatter = require('front-matter');
+const FileSystem = require('fs');
+const R = require('ramda');
+const Async = require('async');
+const Crypto = require('crypto');
+
+const files = R.curryN(2, FileSystem.readdir)('articles');
+const loadArticle = (name, cb) =>
+  FileSystem.readFile("articles/" + name, "utf-8", cb);
+const load = (names, cb) =>
+  Async.map(names, loadArticle, cb);
+const hash = data =>
+  Crypto.createHash('md5').update(data).digest('hex');
+const addPublicFile = a =>
+    R.assoc('publicFile', hash(a.body) + '.md', a);
+const interpret =
+  Async.asyncify(R.map(R.compose(addPublicFile,FrontMatter)));
+const writeArticle = (article, cb) => {
+  console.log(article.publicFile);
+  console.log(article.body);
+  FileSystem.writeFile("public/" + article.publicFile, article.body, cb);
+}
+const write = (articles, cb) => {
+  Async.map(articles, writeArticle, (err, res) => {
+    if (err)
+      cb(err);
+    else
+      cb(null, articles);
+  });
+};
+const clean =
+  Async.asyncify(
+    R.map(
+      R.compose(
+        a => a.attributes,
+        R.assoc('publicFile',undefined),
+        R.assoc('body', undefined),
+        R.assoc('frontmatter', undefined))));
+
+Async.waterfall([
+  files,
+  load,
+  interpret,
+  write,
+  clean
+], (err, res) => {
+  console.log("err", err);
+  console.log("res", res);
+});
