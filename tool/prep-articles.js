4c9a3da0a8000349a1795177454df4f642a8c377
Greg Wiley <aztec.rex@jammm.com>
2017-01-20 22:55:31 -0800

    more progress on article rewrite

--- tool/prep-articles.js
+++ tool/prep-articles.js
@@ -3,27 +3,30 @@
 const FrontMatter = require('front-matter');
 const FileSystem = require('fs');
 const R = require('ramda');
+const _ = R.__;
 const Async = require('async');
 const Crypto = require('crypto');
 
-const files = R.curryN(2, FileSystem.readdir)('articles');
+const articleFiles = R.curryN(2, FileSystem.readdir)('articles');
 const loadArticle = (name, cb) =>
   FileSystem.readFile("articles/" + name, "utf-8", cb);
-const load = (names, cb) =>
+const loadArticles = (names, cb) =>
   Async.map(names, loadArticle, cb);
 const hash = data =>
   Crypto.createHash('md5').update(data).digest('hex');
 const addPublicFile = a =>
     R.assoc('source', hash(a.body) + '.md', a);
-const interpret =
+const processArticles =
   Async.asyncify(R.map(R.compose(addPublicFile,FrontMatter)));
 const writeArticle = (article, cb) => {
-  console.log(article.source);
-  console.log(article.body);
-  FileSystem.writeFile("public/" + article.source, article.body, cb);
+  FileSystem.writeFile("public/article/" + article.source, article.body, cb);
 }
-const write = (articles, cb) => {
-  Async.map(articles, writeArticle, (err, res) => {
+const mkdir = R.curryN(3,FileSystem.mkdir)(_, parseInt('0777', 8));
+const writePublic = (articles, cb) => {
+  Async.series([
+    Async.reflect(mkdir('public/article')),
+    R.curry(Async.map)(articles, writeArticle)
+  ],(err, res) => {
     if (err)
       cb(err);
     else
@@ -36,10 +39,10 @@ const clean =
       a => R.assoc('source', a.source, a.attributes)));
 
 Async.waterfall([
-  files,
-  load,
-  interpret,
-  write,
+  articleFiles,
+  loadArticles,
+  processArticles,
+  writePublic,
   clean
 ], (err, res) => {
   console.log("err", err);
