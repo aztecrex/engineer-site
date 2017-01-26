0e7df2ca358303143b4b59fd435cf3feb7d49458
Greg Wiley <aztec.rex@jammm.com>
2017-01-25 21:33:40 -0800

    connect article page

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -1,4 +1,19 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import R from 'ramda';
 
+const ArticlePage = ({params, index}) => {
 
-export default ({params}) => (<div><p>{params.id}</p></div>);
+  const id = params.id;
+  const entry = index[id];
+  const digest = entry.digest;
+
+  return (<div><p>{id} - {digest}</p></div>)
+};
+
+const mapStateToProps = state => {
+  const index = R.path(['articles','index'], state) || {};
+  return {index};
+};
+
+export default connect(mapStateToProps)(ArticlePage);
--- src/components/ArticlePage.test.js
+++ src/components/ArticlePage.test.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
+import {Provider} from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 
 import ArticlePage from './ArticlePage';
 
 it('should display the article id', () => {
-  let id = 'article012345';
-  let params = {id};
-  expect(shallow(<ArticlePage params={params} />).text()).toContain(id);
+
+  const id = 'article012345';
+  const digest = 'digest-for-article-012345';
+  const state = {
+    articles: {
+      index: {
+        [id]: {
+          digest
+        }
+      }
+    }
+  };
+
+  const createMockStore = configureMockStore([]);
+  const store = createMockStore(state);
+
+  const params = {id};
+
+  const embedded = (
+    <Provider store={store}>
+      <ArticlePage params={params} />
+    </Provider>
+  );
+
+  expect(mount(embedded).find(ArticlePage).text()).toContain(id);
+  expect(mount(embedded).find(ArticlePage).text()).toContain(digest);
 });
