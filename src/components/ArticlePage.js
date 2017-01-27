954baf1a49f5ce45318b93405833087cb6f6ed54
Greg Wiley <aztec.rex@jammm.com>
2017-01-26 16:02:09 -0800

    article page unconnected render

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import R from 'ramda';
-import {actions} from '../model';
+import Article from './Article';
+import Harper from './Harper';
 
-const ArticlePage = ({params, index, content, needContent}) => {
+class ArticlePage extends React.Component {
 
+  render() {
+    return (
+      <div className="article-page">
+        <h1>{this.props.title}</h1>
+        <Article id={this.props.id} />
+        <hr />
+        <Harper />
+      </div>
+    );
+  }
+
+}
+
+const mapStateToProps = (state, {params}) => {
   const id = params.id;
-  const entry = index[id];
-  const digest = entry.digest;
-  let article;
-  if (digest in content)
-    article = content[digest];
-  else
-    needContent(digest);
-  return (
-    <div>
-      <p>{id} - {digest}</p>
-      <p>{article || ""}</p>
-    </div>
-  );
+  const title = R.path(['articles',[id],'title'],state);
+  return {id,title};
 };
 
-const mapStateToProps = state => {
-  const index = R.path(['articles','index'], state) || {};
-  const content = R.path(['articles','content'], state) || {};
-  return {index, content};
-};
-
-export default connect(mapStateToProps, actions)(ArticlePage);
+export default connect(mapStateToProps)(ArticlePage);
+export {ArticlePage, mapStateToProps};
--- src/components/ArticlePage.test.js
+++ src/components/ArticlePage.test.js
@@ -1,50 +1,66 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
-import {Provider} from 'react-redux';
+import ezJson from 'enzyme-to-json';
 import configureMockStore from 'redux-mock-store';
-import {actions} from '../model';
-const {needContent} = actions;
 
-import ArticlePage from './ArticlePage';
+import ConnectedArticlePage, {ArticlePage,mapStateToProps} from './ArticlePage';
 
-const id = 'article012345';
-const digest = 'digest-for-article-012345';
-const state = {
-  articles: {
-    index: {
-      [id]: {
-        digest
-      }
-    }
-  }
-};
-const createMockStore = configureMockStore([]);
+describe('render unconnected', () => {
 
-it('should display the article id', () => {
+  it('renders', () => {
+    // given
+    const id = 'article309';
+    const title = 'This is SO  Neat';
 
-  const store = createMockStore(state);
-  const params = {id};
-  const embedded = (
-    <Provider store={store}>
-      <ArticlePage params={params} />
-    </Provider>
-  );
+    // when
+    const rendered = shallow(<ArticlePage {...{id,title}} />);
 
-  expect(mount(embedded).find(ArticlePage).text()).toContain(id);
-  expect(mount(embedded).find(ArticlePage).text()).toContain(digest);
+    // then
+    expect(ezJson(rendered)).toMatchSnapshot();
+
+  });
 });
 
-it('should emit request for missing content', () => {
-  const store = createMockStore(state);
-  const params = {id};
-  const embedded = (
-    <Provider store={store}>
-      <ArticlePage params={params} />
-    </Provider>
-  );
 
-  mount(embedded);
 
-  expect(store.getActions()[0]).toEqual(needContent(digest));
 
-});
+// const id = 'article012345';
+// const digest = 'digest-for-article-012345';
+// const state = {
+//   articles: {
+//     index: {
+//       [id]: {
+//         digest
+//       }
+//     }
+//   }
+// };
+// const createMockStore = configureMockStore([]);
+//
+// it('should display the article id', () => {
+//
+//   const store = createMockStore(state);
+//   const params = {id};
+//   const embedded = (
+//     <Provider store={store}>
+//       <ArticlePage params={params} />
+//     </Provider>
+//   );
+//
+//   expect(mount(embedded).find(ArticlePage).text()).toContain(id);
+//   expect(mount(embedded).find(ArticlePage).text()).toContain(digest);
+// });
+//
+// it('should emit request for missing content', () => {
+//   const store = createMockStore(state);
+//   const params = {id};
+//   const embedded = (
+//     <Provider store={store}>
+//       <ArticlePage params={params} />
+//     </Provider>
+//   );
+//
+//   mount(embedded);
+//
+//   expect(store.getActions()[0]).toEqual(needContent(digest));
+//
+// });
