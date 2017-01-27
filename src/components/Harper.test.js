aa0b6810b115da5f57c80b48249c89c7b67e6929
Greg Wiley <aztec.rex@jammm.com>
2017-01-26 19:25:22 -0800

    harper click

--- src/components/Harper.test.js
+++ src/components/Harper.test.js
@@ -84,52 +84,19 @@ describe('connected', () => {
 
   });
 
+  it('issues action on click', () => {
+
+    // given
+    const store = createMockStore({});
+    const rendered = mount(<ConnectedHarper store={store} />);
+
+    // when
+    rendered.simulate('click');
+
+    // then
+    expect(store.getActions().length).toEqual(1);
+    expect(store.getActions()[0]).toEqual(flipName());
+
+  });
+
 });
-
-
-// const makeState = name => {
-//   return {name: name};
-// };
-//
-// const embed = (component, store) => {
-//   return (
-//     <Provider store={store}>
-//       {React.createElement(component)}
-//     </Provider>
-//   );
-// };
-//
-//
-// it ('renders name in a span', () => {
-//   // given
-//   let name = 'Charles';
-//   let state = makeState(name);
-//   let store = createMockStore(state);
-//   let embedded = embed(Harper, store);
-//
-//   // when
-//   let wrapper = render(embedded);
-//
-//   // then
-//   let spans = wrapper.find('span');
-//   expect(spans.length).toBe(1);
-//   expect(wrapper.text()).toEqual(name);
-//
-// });
-//
-// it ('emits a flip name action on click', () => {
-//
-//   // given
-//   let state = makeState('Marla');
-//   let store = createMockStore(state);
-//   let embedded = embed(Harper, store);
-//   let wrapper = mount(embedded);
-//
-//   // when
-//   wrapper.simulate('click');
-//
-//   // then
-//   expect(store.getActions()[0]).toEqual(flipName());
-//
-//
-// });
