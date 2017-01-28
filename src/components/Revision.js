504a0943f4579e47b24ceeee269b51d4c61785f4
Greg Wiley <aztec.rex@jammm.com>
2017-01-28 14:51:39 -0800

    start of new araticle

--- src/components/Revision.js
+++ src/components/Revision.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Revision extends React.Component {
   render() {
-    return <span className='revision'>2</span>
+    return <span className='revision'>3</span>
   }
 }
 
