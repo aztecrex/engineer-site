4fe1df9ef19dfec43e6f4d95a73bdb33af8ed65d
Greg Wiley <aztec.rex@jammm.com>
2017-01-29 19:52:24 -0800

    remove decrypt traces

--- decrypt.sh
+++ decrypt.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-echo "[TRACE] starting decrypt"
-
 plainfile=$(mktemp)
 binfile=$(mktemp)
 
-echo "[TRACE] binfile=${binfile}"
-echo "[TRACE] plainfile=${plainfile}"
-
 < ${1}.encrypted > ${binfile} base64 -D
-
-echo "[TRACE] converted to binary"
 
 aws kms decrypt \
   --ciphertext-blob fileb://${binfile} \
@@ -19,12 +12,6 @@ aws kms decrypt \
   | base64 -D \
   | tee ${plainfile}
 
-echo "[TRACE] decrypted"
-
 mv ${plainfile} ${1}
 
-echo "[TRACE] moved"
-
 rm -f ${binfile}
-
-echo "[TRACE] deleted"
