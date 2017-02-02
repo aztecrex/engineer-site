50da2ed7800cd0aaa6d461f49852e5be0b1162bb
Greg Wiley <aztec.rex@jammm.com>
2017-02-01 21:45:14 -0800

    righthand nav on homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Flexbox from 'flexbox-react';
 import LatestArticle from './LatestArticle';
 import Directory from './Directory';
 import Harper from './Harper';
@@ -8,19 +9,33 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className="page home">
-        <Directory />
-        <hr />
-        <LatestArticle />
-        <hr />
-        <Directory />
-        <hr />
-        <Harper />
-        <hr />
-        <div>
-          <p>
-            Revision <Revision />
-          </p>
-        </div>
+        <Flexbox flexDirection="row">
+          <Flexbox flexDirection="column" >
+
+            <Flexbox element="header">
+              <span id="title">Engineer Site</span>
+            </Flexbox>
+            <hr />
+            <Flexbox flexGrow={1}>
+              <LatestArticle />
+            </Flexbox>
+
+            <hr />
+
+            <Flexbox element="footer">
+              <Harper />
+              <hr />
+              <div>
+                <p>
+                  Revision <Revision />
+                </p>
+              </div>
+            </Flexbox>
+          </Flexbox>
+          <Flexbox width="50em">
+            <Directory />
+          </Flexbox>
+        </Flexbox>
       </div>
     );
   }
