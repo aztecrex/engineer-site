7a62a63cdf62993a59755fb07c63870108c2ae33
Greg Wiley <gwiley@cj.com>
2017-02-25 13:41:39 -0800

    rethink encryption commands

--- decrypt.sh
+++ decrypt.sh
@@ -1,25 +1,60 @@
 #!/bin/bash
 
-uname=$(uname);
-
-if [ "$uname" = "Darwin" ]; then
+if [ "$(uname)" = "Darwin" ]; then
   decode_opt="-D"
 else
   decode_opt="-d"
 fi
 
-plainfile=$(mktemp)
-binfile=$(mktemp)
+decode() {
+  base64 "$decode_opt"
+}
 
-< ${1}.encrypted > ${binfile} base64 "$decode_opt"
+usage() {
+  echo "Usage $0 " \
+    "[--profile <aws-profile>] " \
+    "[--output <output-file>] " \
+    "[<input-file>] " \
+    >&2
+}
 
-aws kms decrypt \
-  --ciphertext-blob fileb://${binfile} \
-  --query Plaintext \
-  --output text \
-  | base64 "$decode_opt" \
-  > ${plainfile}
+aws_opts=()
+input_file=
+output_file=
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    --help|help|-h)
+      usage
+      exit 3
+      ;;
+    --profile|-p)
+      aws_opts+=(--profile "$2")
+      shift
+      ;;
+    --output|-o)
+      output_file="$2"
+      shift
+      ;;
+    *)
+      if [ "$input_file" = "" ]; then
+        input_file="$1"
+      else
+        echo "Unrecognized option '${key}'" >&2
+        usage
+        exit 2
+      fi
+      ;;
+  esac
+  shift
+done
+input_file="${input_file:-/dev/stdin}"
 
-mv ${plainfile} ${1}
-
-rm -f ${binfile}
+cat "$input_file" | \
+  decode | \
+  aws ${aws_opts[@]} kms decrypt \
+    --ciphertext-blob fileb:///dev/stdin\
+    --query Plaintext \
+    --output text | \
+  decode \
+  > "${output_file:-/dev/stdout}"
